983e9de0086567530c00818f9340068ae2045f9d
louis.klt <kha.klt@byteuptech.com>
2024-11-17 10:39:52 +0700

    Basic configuration: Add spring boot setup

---
+++ source-code/kafka-deep-dive/src/main/java/kafka/deep/dive/KafkaDeepDiveApplication.java
@@ -0,0 +1,13 @@
+package kafka.deep.dive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDeepDiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaDeepDiveApplication.class, args);
+    }
+
+}
---
+++ source-code/kafka-deep-dive/src/test/java/kafka/deep/dive/KafkaDeepDiveApplicationTests.java
@@ -0,0 +1,13 @@
+package kafka.deep.dive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDeepDiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
