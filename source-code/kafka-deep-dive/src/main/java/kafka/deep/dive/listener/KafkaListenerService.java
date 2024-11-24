b052425a9b4cbb6c1b01b74449cec3553725230d
louis.klt <kha.klt@byteuptech.com>
2024-11-24 17:35:59 +0700

    change to use log4j

--- source-code/kafka-deep-dive/src/main/java/kafka/deep/dive/listener/KafkaListenerService.java
+++ source-code/kafka-deep-dive/src/main/java/kafka/deep/dive/listener/KafkaListenerService.java
@@ -1,5 +1,6 @@
 package kafka.deep.dive.listener;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -7,19 +8,18 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class KafkaListenerService {
 
     @KafkaListener(topics = "sampleTopic", groupId = "spring.kafka.consumer.group-id")
     public void listenGroupFoo(String message) {
-        System.out.println("Received Message in group foo: " + message);
+        log.debug("Received Message in group foo: {}", message);
     }
 
     @KafkaListener(topics = "topicName")
     public void listenWithHeaders(
             @Payload String message,
             @Header(KafkaHeaders.RECEIVED) int partition) {
-        System.out.println(
-                "Received Message: " + message
-                        + "from partition: " + partition);
+        log.debug("Received Message: {}from partition: {}", message, partition);
     }
 }
